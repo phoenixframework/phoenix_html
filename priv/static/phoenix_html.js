bc208c5437bbf0177ebe6770cc55e5e31ba04e22
José Valim <jose.valim@plataformatec.com.br>
2015-07-30 17:56:19 +0200

    Use event argument instead of implicit window.event

--- priv/static/phoenix_html.js
+++ priv/static/phoenix_html.js
@@ -6,7 +6,7 @@ var elements = document.querySelectorAll('[data-submit^=parent]');
 var len = elements.length;
 
 for (var i = 0; i < len; ++i) {
-  elements[i].addEventListener('click', function () {
+  elements[i].addEventListener('click', function (event) {
     var message = this.getAttribute("data-confirm");
     if (message === null || confirm(message)) {
       this.parentNode.submit();
