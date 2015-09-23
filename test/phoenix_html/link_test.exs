1adc61df194f1d27d2725a836d51c2b8ce4446eb
Matt Widmann <mgwidmann@gmail.com>
2015-09-23 10:30:37 -0400

    Handle nil value in to option with better error message

--- test/phoenix_html/link_test.exs
+++ test/phoenix_html/link_test.exs
@@ -34,7 +34,7 @@ defmodule Phoenix.HTML.LinkTest do
   end
 
   test "link with invalid args" do
-    msg = "option :to is required in link/2"
+    msg = "expected non-nil value for :to in link/2, got: nil"
     assert_raise ArgumentError, msg, fn ->
       link("foo", [bar: "baz"])
     end
