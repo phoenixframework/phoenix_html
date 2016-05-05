7fd5a6c8268b5058a0ec2186e187ec6a964bcc7c
arpieb <rob@octobang.com>
2016-05-05 14:39:17 -0400

    First pass at adding Phoenix.HTML.Form.help_text to generate tag for form field help text

--- lib/phoenix_html/form.ex
+++ lib/phoenix_html/form.ex
@@ -1141,4 +1141,47 @@ defmodule Phoenix.HTML.Form do
   def field_name(name, field) when is_atom(name),
     do: "#{name}[#{field}]"
 
+  @doc """
+  Generates standard markup for field help text.
+
+  The form should either be a `Phoenix.HTML.Form` emitted
+  by `form_for` or an atom.
+
+  All given options are forwarded to the underlying tag.
+
+  ## Examples
+
+      help_text("Please provide your full name.")
+      #=> <small>Please provide your full name.</small>
+
+      help_text("Please provide your email.", class: "text-muted")
+      #=> <small class="text-muted">Please provide your email.</small>
+
+      help_text do
+        "Please provide your email."
+      end
+      #=> <small>Please provide your email.</small>
+
+      help_text class: "text-muted" do
+        "Please provide your email."
+      end
+      #=> <small class="text-muted">Please provide your email.</small>
+  """
+  def help_text(text) when is_binary(text) do
+    help_text(text, [])
+  end
+  def help_text([do: block]) do
+    help_text([], do: block)
+  end
+
+  @doc """
+  See `help_text/1`.
+  """
+  def help_text(text, opts) when is_binary(text) and is_list(opts) do
+    content_tag(:small, text, opts)
+  end
+  def help_text(opts, [do: block]) do
+    content_tag(:small, opts, do: block)
+  end
+
 end
