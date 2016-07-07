986d7cace1d4250376c901d89f268e6a7fcd678f
ian asaff <ian.asaff@gmail.com>
2016-07-07 06:09:54 -0400

    handle elements nested inside data-submit anchor tags

--- priv/static/phoenix_html.js
+++ priv/static/phoenix_html.js
@@ -1,11 +1,23 @@
 'use strict';
 
+function didHandleSubmitLinkClick(element){
+  while(element) {
+    if(element.matches && element.matches('a[data-submit=parent]')){
+      var message = element.getAttribute('data-confirm');
+      if (message === null || confirm(message)) {
+        element.parentNode.submit();
+      };
+      return true;
+    } else {
+      element = element.parentNode;
+    }
+  }
+  return false;
+}
+
+// for links with HTTP methods other than GET
 window.addEventListener('click', function (event) {
-  if(event.target && event.target.matches('a[data-submit=parent]')) {
-    var message = event.target.getAttribute('data-confirm');
-    if (message === null || confirm(message)) {
-      event.target.parentNode.submit();
-    };
+  if(event.target && didHandleSubmitLinkClick(event.target)) {
     event.preventDefault();
     return false;
   }
