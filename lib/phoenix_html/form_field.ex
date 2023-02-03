0a6d8e33b30a298e53582d94edfb3483879292e0
Michael Crumm <michael.crumm@cargosense.com>
2023-02-03 15:43:58 -0800

    Add field to FormField struct (#404)

--- lib/phoenix_html/form_field.ex
+++ lib/phoenix_html/form_field.ex
@@ -5,12 +5,13 @@ defmodule Phoenix.HTML.FormField do
   It has the following fields:
 
     * `:errors` - a list of errors belonging to the field
+    * `:field` - the field name as an atom
     * `:form` - the parent `form` struct
     * `:id` - the `id` to be used as form input as a string
     * `:name` - the `name` to be used as form input as a string
     * `:value` - the value for the input
 
   """
-  @enforce_keys [:id, :name, :errors, :form, :value]
-  defstruct [:id, :name, :errors, :form, :value]
+  @enforce_keys [:id, :name, :errors, :field, :form, :value]
+  defstruct [:id, :name, :errors, :field, :form, :value]
 end
